0c391574aed3682e097dbed06e4c5dfb20939166
Faster Speeding <luke@lmbyrne.dev>
2021-06-03 15:41:41 +0100

    Switch GatewayGuildDefinition to using getter style methods for delaying deserialization

--- hikari/impl/event_factory.py
+++ hikari/impl/event_factory.py
@@ -201,23 +201,16 @@ class EventFactoryImpl(event_factory.EventFactory):
     def deserialize_guild_create_event(
         self, shard: gateway_shard.GatewayShard, payload: data_binding.JSONObject
     ) -> guild_events.GuildAvailableEvent:
-        guild_information = self._app.entity_factory.deserialize_gateway_guild(payload).parse_all()
-        assert guild_information.guild is not None
-        assert guild_information.channels is not None
-        assert guild_information.emojis is not None
-        assert guild_information.members is not None
-        assert guild_information.presences is not None
-        assert guild_information.roles is not None
-        assert guild_information.voice_states is not None
+        guild_information = self._app.entity_factory.deserialize_gateway_guild(payload)
         return guild_events.GuildAvailableEvent(
             shard=shard,
-            guild=guild_information.guild,
-            emojis=guild_information.emojis,
-            roles=guild_information.roles,
-            channels=guild_information.channels,
-            members=guild_information.members,
-            presences=guild_information.presences,
-            voice_states=guild_information.voice_states,
+            guild=guild_information.guild(),
+            emojis=guild_information.emojis(),
+            roles=guild_information.roles(),
+            channels=guild_information.channels(),
+            members=guild_information.members(),
+            presences=guild_information.presences(),
+            voice_states=guild_information.voice_states(),
         )
 
     def deserialize_guild_update_event(
@@ -227,17 +220,12 @@ class EventFactoryImpl(event_factory.EventFactory):
         *,
         old_guild: typing.Optional[guild_models.Guild],
     ) -> guild_events.GuildUpdateEvent:
-        guild_information = (
-            self._app.entity_factory.deserialize_gateway_guild(payload).parse_guild().parse_emojis().parse_roles()
-        )
-        assert guild_information.guild is not None
-        assert guild_information.emojis is not None
-        assert guild_information.roles is not None
+        guild_information = self._app.entity_factory.deserialize_gateway_guild(payload)
         return guild_events.GuildUpdateEvent(
             shard=shard,
-            guild=guild_information.guild,
-            emojis=guild_information.emojis,
-            roles=guild_information.roles,
+            guild=guild_information.guild(),
+            emojis=guild_information.emojis(),
+            roles=guild_information.roles(),
             old_guild=old_guild,
         )
 
